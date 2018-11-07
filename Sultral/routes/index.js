cae53d40593d00965b326ac4d5f507e26c254768
Venrique <37162608+Venrique@users.noreply.github.com>
2018-11-07 11:31:30 -0600

    Se agregó la página de Log In y la de Registro. Se añadió el paquete Nodemon para reiniciar el servidor facilmente

--- Sultral/routes/index.js
+++ Sultral/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sultral' });
 });
 
+router.get('/LogIn.pug', function(req, res, next) {
+  res.render('LogIn', { title: 'Sultral - Iniciar Sesión' });
+});
+
+router.get('/Registrar.pug', function(req, res, next) {
+  res.render('Registrar', { title: 'Sultral - Registrarse' });
+});
 module.exports = router;
