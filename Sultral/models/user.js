0b292802a570e3fe41fb3eb24e5d9cd8fbe8286a
Venrique <37162608+Venrique@users.noreply.github.com>
2018-11-11 17:33:07 -0600

    Se terminó por completo la pantalla de registrar junto con sus respectivas validaciones.

--- Sultral/models/user.js
+++ Sultral/models/user.js
@@ -4,7 +4,7 @@ Schema = mongoose.Schema;
 const userSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     user: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     pass: {type: String, required: true}
 });
 
--- Sultral/routes/register.js
+++ Sultral/routes/register.js
@@ -5,56 +5,67 @@ const mongoose = require('mongoose');
 const Usuario = require('../models/user');
 const bcrypt = require('bcrypt');
 
+const regexPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$/;
 
 router.get('/', function (req, res, next) {
     res.render('Registrar', { title: 'Sultral - Registrarse' });
 });
 
 router.post('/', function (req, res, next) {
-    //verifica que no exista el correo en la base de datos
-    Usuario.findOne({ email: req.body.correo })
-        .exec()
-        .then(email => {
-            if (email) {
+    //Verifica que las contraseñas coincidan
+    if (req.body.contrasenia != req.body.confirm) {
+        return res.render('Registrar', { title: 'Sultral - Registrarse', errPass: true, err: '*Las contraseñas no coinciden.', usuario: req.body.usuario, correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm });
+    }else if(!regexPass.test(req.body.contrasenia)){
+        return res.render('Registrar', { title: 'Sultral - Registrarse', errPass: true, err: '*La contraseña debe poseer mínimo 8 caracteres, una letra y un numero.', usuario: req.body.usuario, correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm });
+    } else {
+        //verifica que no exista el correo en la base de datos
+        Usuario.findOne({ email: req.body.correo })
+            .exec()
+            .then(email => {
+                if (email) {
 
-                return res.render('Registrar', { title: 'Sultral - Registrarse',errEmail: true,err: '*El correo se encuentra en uso por otro usuario.',usuario: req.body.usuario,correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm });
-                /*return res.status(409).json({
-                    message: 'El correo se encuentra en uso por otro usuario.'
-                });*/
-            } else {
-                //se encripta la contrasenia
-                bcrypt.hash(req.body.contrasenia, 10, (err, hash) => {
-                    //si da error el encriptado
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                        //se crea el usuario usando el modelo
-                    } else {
-                        const usuario = new Usuario({
-                            _id: new mongoose.Types.ObjectId(),
-                            user: req.body.usuario,
-                            email: req.body.correo,
-                            pass: hash
-                        });
-                        //se guarda el usuario en la base de datos
-                        usuario.save().then(result => {
-                            console.log(result);
+                    return res.render('Registrar', { title: 'Sultral - Registrarse', errEmail: true, err: '*El correo ya está registrado.', usuario: req.body.usuario, correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm });
 
-                            res.status(201).json({
-                                message: "Se creo el usuario",
-                                createdUser: usuario
+                } else {
+                    //verifica que no exista el usuario en la base de datos
+                    Usuario.findOne({ user: req.body.usuario }).exec().then(user => {
+                        if (user) {
+                            return res.render('Registrar', { title: 'Sultral - Registrarse', errUser: true, err: '*El usuario no está disponible.', usuario: req.body.usuario, correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm });
+                        } else {
+                            //se encripta la contrasenia
+                            bcrypt.hash(req.body.contrasenia, 10, (err, hash) => {
+                                //si da error el encriptado
+                                if (err) {
+                                    return res.status(500).json({
+                                        error: err
+                                    });
+                                    //se crea el usuario usando el modelo
+                                } else {
+                                    const usuario = new Usuario({
+                                        _id: new mongoose.Types.ObjectId(),
+                                        user: req.body.usuario,
+                                        email: req.body.correo,
+                                        pass: hash
+                                    });
+                                    //se guarda el usuario en la base de datos
+                                    usuario.save().then(result => {
+                                        console.log(result);
+                                        return res.render('LogIn', { title: 'Sultral - Iniciar Sesión' });
+                                    }).catch(error => {
+                                        console.log(error);
+                                        return res.render('Registrar', {
+                                            title: 'Sultral - Registrarse',
+                                            err: '*Error de validación. Por favor revise que todos los campos estén completos y que la contraseña posea mínimo 8 caracteres y por lo menos una letra y un número.',
+                                            usuario: req.body.usuario, correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm
+                                        });
+                                    });
+                                }
                             });
-
-                            return res.render('LogIn', { title: 'Sultral - Iniciar Sesión'});
-                        }).catch(error => {
-                            console.log(error);
-                            return res.render('Registrar', { title: 'Sultral - Registrarse',err: '*No puede dejar campos vacíos.',usuario: req.body.usuario,correo: req.body.correo, pass: req.body.contrasenia, passconfirm: req.body.confirm });
-                        });
-                    }
-                });
-            }
-        })
+                        }
+                    });
+                }
+            });
+    }
 });
 
 module.exports = router;
