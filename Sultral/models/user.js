f6283aee079a61d98ad5924865a22c6ba834ca93
Venrique <37162608+Venrique@users.noreply.github.com>
2018-11-26 16:46:04 -0600

    Se eliminó una versión de nodemon posiblemente maliciosa y se volvió a instalar

--- Sultral/models/user.js
+++ Sultral/models/user.js
@@ -3,7 +3,7 @@ Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user: {type: String, required: true},
+    user: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     pass: {type: String, required: true}
 });
