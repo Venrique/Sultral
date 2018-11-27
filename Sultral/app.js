ba07af4f07de2a6f177ae2b51f6593bd45ded1cd
Mario <00046317@uca.edu.sv>
2018-11-27 13:07:11 -0600

    Logos agregados y error 404 personalizado

--- Sultral/app.js
+++ Sultral/app.js
@@ -50,6 +50,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  res.status(404);
+  res.render('Not-Found', { title: 'Sultral'});
   res.status(err.status || 500);
   res.render('error');
 });
