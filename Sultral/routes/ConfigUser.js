f34344781e4b51b124a7e9f43fbbcde14b4739b8
RickyGomez10 <00108517@uca.edu.sv>
2018-11-26 19:01:11 -0600

    Agregado Cambiar contraseña

--- Sultral/routes/ConfigUser.js
+++ Sultral/routes/ConfigUser.js
@@ -1,9 +1,30 @@
 var express = require('express');
+
 var router = express.Router();
+
+const mongoose = require('mongoose');
+const Usuario = require('../models/user');
+const bcrypt = require('bcrypt');
 
 
 router.get('/', function(req, res, next) {
+    //console.log(req.params.hola);
     res.render('Config-usuario', { title: 'Configuracion Usuario' });
 });
 
+router.post('/', function (req, res, next) {
+
+    bcrypt.hash(req.body.nueva, 10, (err, hash) => {
+        //si da error el encriptado
+        Usuario.findOneAndUpdate({user: "ricardoDef"}, {pass: hash}, function (err, place) {
+            res.send(place);
+            console.log(place.pass);
+          });
+    
+            });
+
+ 
+
+
+ });
 module.exports = router;
