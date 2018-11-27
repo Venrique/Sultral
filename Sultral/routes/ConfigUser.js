1c256f60fe17838e29a81d67c1b51c673e4d4097
RickyGomez10 <00108517@uca.edu.sv>
2018-11-27 16:06:33 -0600

    Css agregado

--- Sultral/routes/ConfigUser.js
+++ Sultral/routes/ConfigUser.js
@@ -13,31 +13,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+    if(req.body.nueva && req.body.confirmacion){
+        if(req.body.nueva == req.body.confirmacion){
+            bcrypt.hash(req.body.nueva, 10, (err, hash) => {
+                //si da error el encriptado
+                Usuario.findOneAndUpdate({user: "RicardoAlberto"}, {pass: hash}, function (err, place) {
+                    res.send(place);
+                    console.log(place.pass);
+                  });
+            
+                    });
 
-    bcrypt.hash(req.body.nueva, 10, (err, hash) => {
-        //si da error el encriptado
-        Usuario.findOneAndUpdate({user: "ricardoDef"}, {pass: hash}, function (err, place) {
-            res.send(place);
-            console.log(place.pass);
-          });
+        }else{
+            console.log("Las contraseñas no coinciden");
+            return res.render('Config-usuario', { title: 'Configuracion Usuario' , err: "La contraseña nueva no coincide con la que usted esta ingresando en la confirmacion"});
+        }
+    }
     
-            });
 
  
 
 
- });
-
- router.put('/', function (req, res, next) {
-
-    bcrypt.hash(req.body.nueva, 10, (err, hash) => {
-        //si da error el encriptado
-        Usuario.findOneAndUpdate({user: "ricardoDef"}, {pass: hash}, function (err, place) {
-            res.send(place);
-            console.log(place.pass);
-          });
-    
-            });
  });
 
 module.exports = router;
