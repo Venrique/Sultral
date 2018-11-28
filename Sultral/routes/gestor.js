93312447c4f2c65ce8333b7d6680bb0fb286cbeb
Venrique <00046517@uca.edu.sv>
2018-11-28 17:38:05 -0600

    Se pueden subir archivos al sistema y estos se guardan en la carpeta del proyecto, además su ubicación se guarda en mongodb

--- Sultral/routes/gestor.js
+++ Sultral/routes/gestor.js
@@ -4,61 +4,63 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Element = require('../models/element');
 const jwt = require('jsonwebtoken');
+const upload = require('express-fileupload');
+router.use(upload());
 
 /* GET home page. */
-router.get('/:loc', function(req, res, next) {
+router.get('/:loc', function (req, res, next) {
 
   const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
   let contFolders;
   let files, actual, container;
-  
-  Element.find({"creador": mongoose.Types.ObjectId(decode.Id), "contenedor": null})
-  .sort( { nombre: 1 } )
-  .exec().then((result) =>{
-    contFolders = result;
 
-    Element.find({"contenedor": mongoose.Types.ObjectId(req.params.loc)})
-    .sort( { nombre: 1 } )
-    .exec().then((result) =>{
-      files = result;
+  Element.find({ "creador": mongoose.Types.ObjectId(decode.Id), "contenedor": null })
+    .sort({ nombre: 1 })
+    .exec().then((result) => {
+      contFolders = result;
 
-      Element.find({"_id": mongoose.Types.ObjectId(req.params.loc)})
-      .sort( { nombre: 1 } )
-      .exec().then((result) =>{
-        actual = result;
+      Element.find({ "contenedor": mongoose.Types.ObjectId(req.params.loc) })
+        .sort({ nombre: 1 })
+        .exec().then((result) => {
+          files = result;
 
-        Element.find({"contenido": mongoose.Types.ObjectId(req.params.loc)})
-        .sort( { nombre: 1 } )
-        .exec().then((result) =>{
-          container = result;
+          Element.find({ "_id": mongoose.Types.ObjectId(req.params.loc) })
+            .sort({ nombre: 1 })
+            .exec().then((result) => {
+              actual = result;
 
-          return res.render('Gestor', { title: 'Sultral', varLoc: req.params.loc, carpetasOrigen: JSON.stringify(contFolders), contenido: JSON.stringify(files), actual: JSON.stringify(actual), contenedor: JSON.stringify(container)});
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+              Element.find({ "contenido": mongoose.Types.ObjectId(req.params.loc) })
+                .sort({ nombre: 1 })
+                .exec().then((result) => {
+                  container = result;
+
+                  return res.render('Gestor', { title: 'Sultral', varLoc: req.params.loc, carpetasOrigen: JSON.stringify(contFolders), contenido: JSON.stringify(files), actual: JSON.stringify(actual), contenedor: JSON.stringify(container) });
+
+                });
+
+            });
+
         });
-
-      });
 
     });
 
-  });
-
 });
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
   let folders;
-  
-  Element.find({"creador": mongoose.Types.ObjectId(decode.Id), "contenedor": null})
-  .sort( { nombre: 1 } )
-  .exec().then((result) =>{
-    folders = result;
-    return res.render('Gestor', { title: 'Sultral', varLoc: "-", carpetasOrigen: JSON.stringify(folders)});
-  });
-  
+
+  Element.find({ "creador": mongoose.Types.ObjectId(decode.Id), "contenedor": null })
+    .sort({ nombre: 1 })
+    .exec().then((result) => {
+      folders = result;
+      return res.render('Gestor', { title: 'Sultral', varLoc: "-", carpetasOrigen: JSON.stringify(folders) });
+    });
+
 });
 
-router.post('/:loc', function(req, res, next){
+router.post('/:loc', function (req, res, next) {
   const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
   let idCarpeta = new mongoose.Types.ObjectId();
 
@@ -74,17 +76,57 @@ router.post('/:loc', function(req, res, next){
 
   carpeta.save().then(result => {
     console.log(result);
-    Element.findOneAndUpdate({_id: mongoose.Types.ObjectId(req.params.loc)}, {$push: { contenido: idCarpeta}}, function(err, place){
-      if(err){
+    Element.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.params.loc) }, { $push: { contenido: idCarpeta } }, function (err, place) {
+      if (err) {
         console.log(err);
       }
     });
-    return res.status(200).redirect('/Gestor/'+req.params.loc);
+    return res.status(200).redirect('/Gestor/' + req.params.loc);
   }).catch(error => {
     console.log(error);
     return res.status(400);
   });
 
 });
+
+router.post('/:loc/upload', function (req, res, next) {
+  let archivo = req.files.finame;
+  const fileId = new mongoose.Types.ObjectId();
+
+  let nombref = req.files.finame.name.split(".");
+
+  const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
+
+  archivo.mv('Sultral/../User_files/' + fileId + '.sultral', function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      const archivoup = new Element({
+        _id: fileId,
+        nombre: nombref[0],
+        ext: nombref[1],
+        contenedor: req.params.loc,
+        contenido: null,
+        creador: mongoose.Types.ObjectId(decode.Id),
+        compartido: []
+      });
+
+      archivoup.save().then(result => {
+        console.log(result);
+        Element.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.params.loc) }, { $push: { contenido: fileId } }, function (err, place) {
+          if (err) {
+            console.log(err);
+          }
+        });
+        return res.status(200).redirect('/Gestor/' + req.params.loc);
+      }).catch(error => {
+        console.log(error);
+        return res.status(400);
+      });
+    }
+
+  });
+});
+
 
 module.exports = router;
