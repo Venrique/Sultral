146f595cdd1167a5fd05a46831107b06e14f8828
Venrique <37162608+Venrique@users.noreply.github.com>
2018-11-27 21:41:11 -0600

    Se modificó la página informátiva dependiendo si el usuario está logueado o no. (commits anteriores sobre tokens son de esta cuenta pero se hicieron por error con otra)

--- Sultral/routes/index.js
+++ Sultral/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sultral' });
+
+  if(req.cookies.token != null){
+    const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
+    console.log(decode);
+    res.render('index', { title: 'Sultral', sesion: true, user: decode.usuario });
+  }else{
+    res.render('index', { title: 'Sultral' });
+  }
 });
 
 router.get('/Cerrar', function(req, res, next) {
