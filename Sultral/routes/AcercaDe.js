e73b6a0197112f7a80892586918611485985f4c5
RickyGomez10 <00108517@uca.edu.sv>
2018-11-29 13:39:40 -0600

    Arreglado contactanos y acerca de

--- Sultral/routes/AcercaDe.js
+++ Sultral/routes/AcercaDe.js
@@ -1,12 +1,22 @@
 var express = require('express');
 
 var router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
 
 
 
 router.get('/', function(req, res, next) {
-    res.render('AcercaDe', { title: 'Sultral - Acerca de' });
-});
+
+    if(req.cookies.token != null){
+      const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
+      console.log(decode);
+      res.render('AcercaDe', { title: 'Sultral - Acerca De', sesion: true, user: decode.usuario });
+    }else{
+      res.render('AcercaDe', { title: 'Sultral - Acerca De ' });
+    }
+  });
 
 
 module.exports = router;
