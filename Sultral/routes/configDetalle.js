e75b9379da3a1d2e95a8dbea708d424b4d98ec35
Mario <00046317@uca.edu.sv>
2018-11-29 16:48:07 -0600

    Gráfico que muestra el almacenamiento utilizado y disponible.

--- Sultral/routes/configDetalle.js
+++ Sultral/routes/configDetalle.js
@@ -1,12 +1,17 @@
 var express = require('express');
 
 var router = express.Router();
-
-
+const Usuario = require('../models/user');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 router.get('/', function(req, res, next) {
-    //console.log(req.params.hola);
-    res.render('ConfigDetalle', { title: 'Configuracion Usuario' });
+    const decode = jwt.decode(req.cookies.token, process.env.JWT_KEY);
+    Usuario.find({_id: mongoose.Types.ObjectId(decode.Id)})
+    .exec().then((result) => {
+        res.render('ConfigDetalle', { title: 'Configuracion Usuario', max: result[0]['maxstorage'], utilizado: result[0]['storage']});
+    });
+    
 });
 
 
